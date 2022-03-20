9e82a5e80cceb5a0dc5e6d8a34077c6efc173e42
stepmurphy1020 <smurphy1020@gmail.com>
2022-03-20 12:17:34 -0400

    Update Poisson.java

--- Statlib/src/Statlib/Poisson.java
+++ Statlib/src/Statlib/Poisson.java
@@ -1,5 +1,13 @@
 package Statlib;
 
 public class Poisson {
-
+	public double pois(double K, double n, double y) {
+		Factorial fac = new Factorial();
+		double l = (K/n); 
+		double a = fac.fac(y);
+		double b = Math.pow(l, y);
+		double e = Math.exp(-a);
+		return (b/a)*e;
+		
+	}
 }

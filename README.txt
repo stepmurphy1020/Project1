1445ed90357f42c0c33e01c7c4082de0c7835abd
stepmurphy1020 <smurphy1020@gmail.com>
2022-03-20 17:22:04 -0400

    edit to the excel

---
+++ README.txt
@@ -0,0 +1,2 @@
+The Excel files relating to the Function plotter/salter/smoother are in their own file called ExcelFiles.
+The .csv files that go with them are in the Function program folder.
--- Statlib/src/Statlib/Main.java
+++ Statlib/src/Statlib/Main.java
@@ -29,15 +29,19 @@ public class Main
     	System.out.println("Standard Deviation:"+sd.SD(array));*/
     	
     	Binomial B = new Binomial();
-    	System.out.println(B.Binomial1(12, .3, .7, 2));
+    	System.out.println("Binomial:");
+    	System.out.println(B.Binomial1(12, .3, .7, 2)+ "\n");
     	
     	Poisson P = new Poisson();
-    	System.out.println(P.pois(4, 2, 4));
+    	System.out.println("Poisson:");
+    	System.out.println(P.pois(4, 2, 4)+ "\n");
     	
     	Hypergeometric H = new Hypergeometric();
-    	System.out.println(H.Hyper(20,5,6,4));
+    	System.out.println("Hypergeometric:");
+    	System.out.println(H.Hyper(20,5,6,4)+ "\n");
     	
     	Geometric G = new Geometric();
-    	System.out.println(G.Geo(.45, .55, 3));
+    	System.out.println("Geometric:");
+    	System.out.println(G.Geo(.45, .55, 3)+ "\n");
     }
 }

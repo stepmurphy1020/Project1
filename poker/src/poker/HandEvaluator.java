3bf2355abeadab07367ed08c94874afd271b0487
stepmurphy1020 <smurphy1020@gmail.com>
2022-03-24 23:58:41 -0400

    Made each method read
    Made each method read through all elements of the hand array

--- poker/src/poker/HandEvaluator.java
+++ poker/src/poker/HandEvaluator.java
@@ -12,28 +12,34 @@ public class HandEvaluator {
 	public static ArrayList<Card> hand = new ArrayList<Card>(4);
 	public void pair() { //this is for comparing two elements
 		int match = 0;
-		for(int i = 0; i<hand.size()-1;i++) {
-			if(hand.get(i).getNumber()==hand.get(i+1).getNumber()){
-				match++;
-				System.out.println(match);
-				if(match<2) {
-					if(match==1) {
-						System.out.println("You have One Pair");
+		for(int i = 0; i<hand.size();i++) {
+			for(int j=i+1; j<hand.size();j++) {
+				if(hand.get(i).getNumber()==hand.get(j).getNumber()){
+					match++;
+					System.out.println(hand.get(i).getNumber()+ ", "+hand.get(j).getNumber());
+					if(match<2) {
+						if(match==1) {
+							System.out.println("You have One Pair");
+					}else {
+						
 					}
 				}
 			}
 		}
 	}
+}
 	
 	public void tok() {
 		int match = 0;
-		for(int i = 0; i<hand.size()-1;i++) {
-			for(int j=1; j<i;j++) {
+		for(int i = 0; i<hand.size();i++) {
+			for(int j=i+1; j<hand.size();j++) {
 				if(hand.get(i).getNumber()==hand.get(j).getNumber()) {
 					match++;
 					if(match<=4) {
 						if(match==3) {
 							System.out.println("You have Three of a Kind");
+						}else {
+							
 						}
 					}
 				}
@@ -44,56 +50,65 @@ public class HandEvaluator {
 	
 	public void twopair() { //checks to see if there are two different pairs
 		int match = 0;
-		for(int i = 0; i<hand.size()-1;i++) {
-			if(hand.get(i).getNumber()==hand.get(i+1).getNumber()){
-				int a = i;
-				match++;
-				if(match==2) {
-						System.out.println("You have Two Pair");
-					}else {
-						System.out.println("You do not have Two Pair");
+		for(int i = 0; i<hand.size();i++) {
+			for(int j=i+1; j<hand.size();j++) {
+				if(hand.get(i).getNumber()==hand.get(i+1).getNumber()){
+					
+					match++;
+					if(match==2) {
+							System.out.println("You have Two Pair");
+						}else {
+					
+						}
 					}
 				}
 			}
 		}
 	
 	public void straight() {//sorts array and checks to see if each card is 1 less than the next card
-		for(int i = 0; i<hand.size()-1;i++) {
-			if(hand.get(i).getNumber()==hand.get(i+1).getNumber()) {
-				}
+		for(int i = 0; i<hand.size();i++) {
+			for(int j=i+1; j<hand.size();j++) {
+				if(hand.get(i).getNumber()==hand.get(i+1).getNumber()) {
+					}
 			}
+		}
 	}
 	/*public void fullhouse() {
 		int match = 0;
-		for(int i = 0; i<hand.size()-1;i++) {
-			if(hand.get(i).getNumber()==hand.get(i+1).getNumber()) {
-				match++;
-				if(match<4) {
-					if(match==3) {
+		for(int i = 0; i<hand.size();i++) {
+			for(int j=i+1; j<hand.size();j++){
+				if(hand.get(i).getNumber()==hand.get(i+1).getNumber()) {
+					match++;
+					if(match<4) {
+						if(match==3) {
 						
 					}
 					
 				}
 			}
 		}
+	}
 		
 	}*/
 	
 	public void flush() {
 		int match = 0;
-		for(int i = 0; i<hand.size()-1;i++) {
+		for(int i = 0; i<hand.size();i++) {
+			for(int j=i+1; j<hand.size();j++) {
 			if(hand.get(i).getSuite()==hand.get(i+1).getSuite()) {
 				match++;
 				if(match==5) {
 					System.out.println("You have a Fush");
 				}
-				}
 			}
 		}
+	}
+}
 
 	public void fok() {
 		int match = 0;
-		for(int i = 0; i<hand.size()-1;i++) {
+		for(int i = 0; i<hand.size();i++) {
+			for(int j=i+1; j<hand.size();j++) {
 			if(hand.get(i).getNumber()==hand.get(i+1).getNumber()) {
 				match++;
 				if(match==4) {
@@ -102,6 +117,6 @@ public class HandEvaluator {
 				}
 			}
 		}
-
+	}
 }
 
